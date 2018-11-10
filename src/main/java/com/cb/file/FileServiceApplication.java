4a2525a77ab18dc21e32ed6cbfd31109369f8e67
cbidici <cbidici@gmail.com>
2018-11-11 00:05:13 +0300

    File table developed.

--- src/main/java/com/cb/file/FileServiceApplication.java
+++ src/main/java/com/cb/file/FileServiceApplication.java
@@ -1,4 +1,4 @@
-package com.cb.service;
+package com.cb.file;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/cb/file/config/FileServiceConfiguration.java
@@ -0,0 +1,8 @@
+package com.cb.file.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FileServiceConfiguration {
+
+}
